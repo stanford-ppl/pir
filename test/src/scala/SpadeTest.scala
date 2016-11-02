75680b1968c9c9d303f3d6893529f35ea2e8e7f1
Yaqi Zhang <hsbzyq@gmail.com>
2016-11-02 06:46:17 -0700

    minor change

--- test/src/scala/SpadeTest.scala
+++ test/src/scala/SpadeTest.scala
@@ -24,21 +24,24 @@ class SpadeTest extends UnitTest { self =>
       case sn:SwitchNetwork =>
         (sn.sbs ++ sn.csbs).flatten.foreach { sb => 
           sb.vins.foreach { vin => 
-            if(vin.fanIns.size>1) 
+            if (vin.fanIns.size>1) { 
+              println(s"Switchbox $sb has $vin with fanIns > 1 ${vin.fanIns}")
               throw PIRException(s"Switchbox $sb has $vin with fanIns > 1 ${vin.fanIns}")
+            }
           }
           sb.vouts.foreach { vout => 
-            if(vout.fanOuts.size>1) 
+            if (vout.fanOuts.size>1) {
               throw PIRException(s"Switchbox $sb has $vout with fanOuts > 1 ${vout.fanOuts}")
+            }
           }
         }
         sn.cus.foreach { pcu =>
           (pcu.vins ++ pcu.cins).foreach { in => 
-            if(in.fanIns.size>1) 
+            if (in.fanIns.size>1) 
               throw PIRException(s"ComputeUnit $pcu has $in with fanIns > 1 ${in.fanIns}")
           }
           pcu.couts.foreach { out => 
-            if(out.fanOuts.size>1) 
+            if (out.fanOuts.size>1) 
               throw PIRException(s"ComputeUnit $pcu has $out with fanOuts > 1 ${out.fanOuts}")
           }
         }
