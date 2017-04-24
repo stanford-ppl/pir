69fdcb64f1759d57c7967f57c54d7dcbdb3c2e34
Yaqi Zhang <hsbzyq@gmail.com>
2017-04-23 22:31:52 -0700

    fix fusiontransform

--- src/pass/FusionTransform.scala
+++ src/pass/FusionTransform.scala
@@ -11,72 +11,80 @@ import scala.collection.mutable.Map
 class FusionTransform(implicit design: Design) extends Pass{
   def shouldRun = true 
 
+  // Handling Remote Copies of counters
+  def swapCopy(parent:OuterController, childCU:ComputeUnit) = {
+    implicit val child:ComputeUnit = childCU 
+    val ccchain = child.localCChain
+    val pcchain = parent.localCChain
+    pcchain.copied.foreach { pcp =>
+      val copiercu = pcp.ctrler.asInstanceOf[InnerController]
+      copiercu match {
+        // Copy outer controller of the writer for write addr calculation
+        case copiercu:MemoryPipeline => 
+          val icc = copiercu.getCopy(ccchain)
+          pcp.counters.reverseIterator.foreach { ctr =>
+            ctr.cchain(icc)
+            icc.addOuterCounter(ctr)
+          }
+          copiercu.removeCChain(pcp)
+
+        // Copy ancesstor outer controller because used in datapath 
+        case copiercu if copiercu.ancestors.contains(parent) =>
+          val ancestors = copiercu.ancestors
+          val pcuPre = ancestors(ancestors.indexOf(parent)-1).asInstanceOf[ComputeUnit]
+          val icc = copiercu.getCopy(pcuPre.localCChain)
+          pcp.counters.reverseIterator.foreach { ctr =>
+            ctr.cchain(icc)
+            icc.addOuterCounter(ctr)
+          }
+          copiercu.removeCChain(pcp)
+      }
+    }
+  }
+  def fuseCUs(parent:OuterController, childCU:ComputeUnit) = {
+    implicit val child:ComputeUnit = childCU 
+    val ccchain = child.localCChain
+    val pcchain = parent.localCChain
+    // Copying from inner most to outer most
+    child match {
+      case child:InnerController => // ccu might already have a copy of pcchain 
+        child.getCopy(pcchain).counters.reverseIterator.foreach { ctr =>
+          ctr.cchain(ccchain)
+          ccchain.addOuterCounter(ctr)
+        }
+        child.removeCChainCopy(pcchain)
+      case chain:OuterController =>
+        pcchain.counters.reverseIterator.foreach { ctr =>
+          val cp = Counter(ccchain)(child, design)
+          cp.copy(ctr)
+          ccchain.addOuterCounter(cp)
+        }
+        child.removeCChainCopy(pcchain)
+        //val iccchain = ccu.inner.getCopy(ccchain)
+        //val ipcchain = ccu.inner.getCopy(pcchain)
+        //ipcchain.counters.reverseIterator.foreach { ctr =>
+          //val cp = Counter(iccchain)(ccu.inner, design)
+          //cp.copy(ctr)
+          //iccchain.addOuterCounter(cp)
+        //}
+        //ccu.inner.removeCChainCopy(pcchain)
+    }
+    child.parent(parent.parent)
+    parent.parent.addChildren(child) 
+    parent.parent.removeChildren(parent)
+    parent.removeChildren(child)
+    parent.removeParent
+    design.removeNode(parent)
+  }
+
   override def traverse:Unit = {
     design.top.outerCUs.foreach { pcu =>
-      // TODO At the moment no need to consider two parallel CUs since multiple last stages are not supported
       if (pcu.children.size==1) {
         pcu.children.head match {
           case sp:StreamPipeline =>
-          case _ =>
-            implicit val ccu:ComputeUnit = pcu.children.head 
-            assert(ccu.isHead && ccu.isLast)
-            val ccchain = ccu.localCChain
-            val pcchain = pcu.localCChain
-            // Handling Remote Copies of counters
-            pcchain.copied.foreach { pcp =>
-              val cpccu = pcp.ctrler.asInstanceOf[InnerController]
-              pcu match {
-                // Copy ancesstor outer controller because used in datapath 
-                case pcu:OuterController if cpccu.ancestors.contains(pcu) => 
-                  val ancestors = cpccu.ancestors
-                  val pcuPre = ancestors(ancestors.indexOf(pcu)-1).asInstanceOf[ComputeUnit]
-                  val icc = cpccu.getCopy(pcuPre.localCChain)
-                  pcp.counters.reverseIterator.foreach { ctr =>
-                    ctr.cchain(icc)
-                    icc.addOuterCounter(ctr)
-                  }
-                  cpccu.removeCChain(pcp)
-                // Copy outer controller of the writer for write addr calculation
-                case cu:OuterController if !cpccu.ancestors.contains(cu) =>
-                  val icc = cpccu.getCopy(ccchain)
-                  pcp.counters.reverseIterator.foreach { ctr =>
-                    ctr.cchain(icc)
-                    icc.addOuterCounter(ctr)
-                  }
-                  cpccu.removeCChain(pcp)
-                case _ =>
-              }
-            }
-            // Copying from inner most to outer most
-            ccu match {
-              case ccu:InnerController => // ccu might already have a copy of pcchain 
-                ccu.getCopy(pcchain).counters.reverseIterator.foreach { ctr =>
-                  ctr.cchain(ccchain)
-                  ccchain.addOuterCounter(ctr)
-                }
-                ccu.removeCChainCopy(pcchain)
-              case ccu:OuterController =>
-                pcchain.counters.reverseIterator.foreach { ctr =>
-                  val cp = Counter(ccchain)(ccu, design)
-                  cp.copy(ctr)
-                  ccchain.addOuterCounter(cp)
-                }
-                ccu.removeCChainCopy(pcchain)
-                //val iccchain = ccu.inner.getCopy(ccchain)
-                //val ipcchain = ccu.inner.getCopy(pcchain)
-                //ipcchain.counters.reverseIterator.foreach { ctr =>
-                  //val cp = Counter(iccchain)(ccu.inner, design)
-                  //cp.copy(ctr)
-                  //iccchain.addOuterCounter(cp)
-                //}
-                //ccu.inner.removeCChainCopy(pcchain)
-            }
-            ccu.parent(pcu.parent)
-            pcu.parent.addChildren(ccu) 
-            pcu.parent.removeChildren(pcu)
-            pcu.removeChildren(ccu)
-            pcu.parent(null)
-            design.removeNode(pcu)
+          case cu => 
+            swapCopy(pcu, cu)
+            fuseCUs(pcu, cu)
         }
       }
     }
