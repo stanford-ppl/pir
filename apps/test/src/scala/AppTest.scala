768bf51df62c3394963bc63bab2188e7e48afb8a
Yaqi Zhang <hsbzyq@gmail.com>
2017-07-18 12:58:03 -0700

    refactoring

--- apps/test/src/scala/AppTest.scala
+++ apps/test/src/scala/AppTest.scala
@@ -184,18 +184,18 @@ class AppTests extends UnitTest { self =>
   //"GDA" should "success" in { GDA.main(Array("GDA")) }
   //"LogReg" should "success" in { LogReg.main(Array("LogReg")) }
   
-  //test(InOutArg_cb, args="x222=4", argOuts="x223_x227=8.0", debug=true)
+  test(InOutArg_cb, args="x222=4", argOuts="x223_x227=8.0", debug=true)
   //test(ParSRAMReadWrite_cb, argOuts="x1026_x1096=10416", timeOut=60, debug=false)
   //test(SimpleSequential_cb, args="x343=2 x342=10", argOuts="x344_x356=2false0", debug=false)
   //test(SimpleSequential_cb, args="x343=1 x342=10", argOuts="x344_x356=10", debug=false)
   //test(SimpleReduce_cb, args="x350=10", argOuts="x351_x365=1200", debug=false)
   //testDotProduct(DotProductSeq_cb, startA=0, startB=16, N=32, debug=false)
   //testDotProduct(DotProductSeq_cb, startA=0, startB=16, N=64, debug=false)
-  testDotProduct(DotProductMeta_cb, startA=0, startB=16, N=32, debug=false)
+  //testDotProduct(DotProductMeta_cb, startA=0, startB=16, N=32, debug=false)
   //testDotProduct(DotProductMeta_cb, startA=0, startB=16, N=64, debug=false)
   //testTPCHQ6(TPCHQ6_cb, startA=0, startB=10, startC=20, startD=30, N=32, debug=false)
   //testTPCHQ6(TPCHQ6_cb, startA=0, startB=10, startC=20, startD=30, N=64, debug=false)
-  //testOuterProduct(OuterProduct_cb, startA=0, startB=100, startC=200, N=16, debug=false)
+  //testOuterProduct(OuterProduct_cb, startA=0, startB=100, startC=200, N=16, debug=true)
   //testMatMult_inner(MatMult_inner, N=16, M=16, P=16, startA=0, startB=20, startC=40, debug=false)
   //testBlockReduce1D(BlockReduce1D, numTile=1, tileSize=16, startSrc=20, startDst=0, debug=true)
   
