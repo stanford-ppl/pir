c2ffab7724b664b2e1698334175ab211536a4711
Yaqi Zhang <hsbzyq@gmail.com>
2018-02-12 17:06:52 -0800

    more working apps

--- pir/apps/test/src/AppTest.scala
+++ pir/apps/test/src/AppTest.scala
@@ -237,12 +237,12 @@ class AppTests extends UnitTest { self =>
   test(OuterProduct       , arch=Some(SN4x4), verbose=verbose, mapping=mapping, debug=true)
   test(TPCHQ6             , arch=Some(SN8x8), verbose=verbose, mapping=mapping, debug=true)
   test(GDA                , arch=None, verbose=verbose, mapping=mapping, debug=true)
+  test(BlackScholes       , arch=Some(SN16x8_LD), verbose=verbose, mapping=mapping, debug=true)
   // Not Working
   //test(SimpleIf           , arch=Some(SN2x2), verbose=verbose, mapping=mapping, debug=true)
   //test(SPMV_CRS           , arch=Some(arch), verbose=verbose, mapping=mapping, debug=true)
   //test(Backprop           , arch=Some(SN8x8), verbose=verbose, mapping=mapping, debug=true)
   //test(Gibbs_Ising2D      , arch=Some(arch), verbose=verbose, mapping=mapping, debug=true)
-  //test(BlackScholes       , arch=Some(SN16x8_LD), verbose=verbose, mapping=mapping, debug=true)
   //test(Kmeans_plasticine  , arch=Some(arch), verbose=verbose, mapping=mapping, debug=true)
   //test(PageRank_plasticine, arch=Some(arch), verbose=verbose, mapping=mapping, debug=true)
   //test(GEMM_Blocked       , arch=Some(arch), verbose=verbose, mapping=mapping, debug=true)
