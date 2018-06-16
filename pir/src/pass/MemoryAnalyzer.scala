53452507ed496e879c209e30a57640e36d88d864
Yaqi Zhang <hsbzyq@gmail.com>
2018-06-15 19:51:27 -0700

    renmove transformer from MemoryAnalyzer

--- pir/src/pass/MemoryAnalyzer.scala
+++ pir/src/pass/MemoryAnalyzer.scala
@@ -5,8 +5,19 @@ import pir.node._
 
 import scala.collection.mutable
 
-class MemoryAnalyzer(implicit compiler:PIR) extends PIRTransformer with SiblingFirstTraversal with UnitTraversal {
+class MemoryAnalyzer(implicit compiler:PIR) extends SiblingFirstTraversal with UnitTraversal {
   import pirmeta._
+
+  override def runPass =  {
+    traverseNode(compiler.top)
+  }
+
+  override def visitNode(n:N) = { 
+    n match {
+      case n:Memory if !ctrlOf.contains(n) => setTopCtrl(n)
+      case n => super.visitNode(n)
+    }
+  }
 
   def setTopCtrl(mem:Memory) = dbgblk(s"setTopCtrl($mem)") {
     val accesses = accessesOf(mem)
@@ -25,40 +36,11 @@ class MemoryAnalyzer(implicit compiler:PIR) extends PIRTransformer with SiblingF
     accesses.foreach { access =>
       val topCtrl = topCtrls(ctrlOf(access))
       val newAccess = access
-      //val newAccess = access match {
-        //case Def(n, LocalStore(mems, addrs, data)) if topCtrlOf.get(access).fold(false){ _ != topCtrl} =>
-          //// store access write to multiple mems with different topControl. Duplicate the access
-          //val accessCU = globalOf(n).get 
-          //dbg(s"disconnecting $n from $mem")
-          //n.outs.foreach { out =>
-            //dbg(s"out ${out} ${out.connected.map(_.src)}")
-          //}
-          //val maccess = mirror(
-            //node=n, 
-            //container=Some(accessCU), 
-            //init=mutable.Map(mems -> List(mem), addrs -> addrs, data -> data),
-            //mirrorRule=PresetRule(n, topCtrlOf, topCtrl)
-          //)
-          //disconnect(n, mem)
-          //dbg(s"duplicating ${qtype(n)} -> ${qtype(maccess)} for different topCtrl")
-          //maccess
-        //case n => n
-      //}
       topCtrlOf(newAccess) = topCtrl
       topCtrlOf.info(newAccess).foreach(dbg)
     }
   }
 
-  override def runPass =  {
-    traverseNode(compiler.top)
-  }
-
-  override def visitNode(n:N) = { 
-    n match {
-      case n:Memory if !ctrlOf.contains(n) => setTopCtrl(n)
-      case n => super.visitNode(n)
-    }
-  }
 
 }
 
