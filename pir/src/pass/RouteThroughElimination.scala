78ce6078f5260a9f4011a11cd2c1c7759d3271ac
Yaqi Zhang <hsbzyq@gmail.com>
2018-09-10 16:40:23 -0700

    add pattern to RouteThroughElimination

--- pir/src/pass/RouteThroughElimination.scala
+++ pir/src/pass/RouteThroughElimination.scala
@@ -14,7 +14,6 @@ class RouteThroughElimination(implicit compiler:PIR) extends PIRTransformer with
     n match {
       // Pattern if write is inside mem CU
       case Def(rwrite:LocalStore, LocalStore(mem::Nil,None,Def(rread, LocalLoad(WithWriter(Def(write, LocalStore(rmem::Nil, None, data)))::Nil,None)))) if matchType(mem, rmem) =>
-        //TODO: check mem and rmem are off the same time. i.e. both reg or both fifo
         dbgblk(s"Found Route Through ${qdef(write)}") {
           dbgs(s"pattern: data -> wirte -> rmem -> rread -> rwrite -> mem => data -> write -> mem")
           dbg(s"data:${qdef(data)}")
@@ -31,6 +30,22 @@ class RouteThroughElimination(implicit compiler:PIR) extends PIRTransformer with
           }
           //swapConnection(mem, rwrite.out, write.out)
           //swapParent(write, memCU)
+        }
+      case Def(rread:LocalLoad, LocalLoad((rmem@WithWriter(Def(rwrite, LocalStore(_, None, Def(read, LocalLoad(mem, None))))))::Nil, None)) if isFIFO(rmem) & ctrlOf(rread) == ctrlOf(read) =>
+        dbgblk(s"Found Route Through ${qdef(rread)}") {
+          dbgs(s"pattern: write -> mem -> read -> rwrite -> rmem -> rread => write -> mem.clone => read.clone")
+          dbg(s"mem:${qdef(mem)}")
+          dbg(s"read:${qdef(read)}")
+          dbg(s"rwrite:${qdef(rwrite)}")
+          dbg(s"rmem:${qdef(rmem)}")
+          dbg(s"rread:${qdef(rread)}")
+          val memCU = globalOf(rmem).get
+          val mread = withParent(memCU) {
+            mirror(read)
+          }
+          rread.deps.foreach { dep =>
+            swapUsage(from=rread, to=mread)
+          }
         }
       // Pattern if write is inside writer CU
       //case Def(rwrite:LocalStore, LocalStore(mems,None,Def(rread, LocalLoad(WithWriter(Def(write, LocalStore(rmem::Nil, None, data)))::Nil,None)))) =>
