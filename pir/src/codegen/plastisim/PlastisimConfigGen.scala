55fc3612e657fa7273ca39673296a8164fecd944
Yaqi Zhang <hsbzyq@gmail.com>
2018-11-26 15:07:05 -0800

    change psim config buffer depth for argFringe and MC

--- pir/src/codegen/plastisim/PlastisimConfigGen.scala
+++ pir/src/codegen/plastisim/PlastisimConfigGen.scala
@@ -152,8 +152,8 @@ class PlastisimConfigGen(implicit compiler: PIR) extends PlastisimCodegen with P
       val tp = if (read.getVec > 1) "vec" else "word"
       paramOf(n) match {
         case param:CUParam => param.fifoParamOf(tp).get.depth
-        case param:ArgFringeParam => 1
-        case param:MCParam => 10
+        case param:ArgFringeParam => 100
+        case param:MCParam => 100
       }
     }
   }
