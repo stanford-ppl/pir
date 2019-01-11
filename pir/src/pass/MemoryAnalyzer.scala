0555704afeae8586533c2e526fc62561562558e7
Yaqi Zhang <hsbzyq@gmail.com>
2019-01-11 13:08:19 -0800

    fixes to compEnqDeq

--- pir/src/pass/MemoryAnalyzer.scala
+++ pir/src/pass/MemoryAnalyzer.scala
@@ -28,30 +28,27 @@ trait MemoryAnalyzer extends PIRPass with Transformer {
 
   def compEnqDeq(a:ControlTree, b:ControlTree, isFIFO:Boolean, actx:Context, bctx:Context):(PIRNode, PIRNode) = {
     dbgblk(s"compEnqDeq($a, $b, isFIFO=$isFIFO, actx=$actx, bctx=$bctx)") {
-      if (a.isAncestorOf(b)) {
-        val bAncesstors = (b::b.ancestors)
-        val idx = bAncesstors.indexOf(a)
-        val ctrl = bAncesstors(idx-1).as[ControlTree]
-        (ctrlValid(a, actx), ctrlDone(ctrl, bctx))
-      } else if (b.isAncestorOf(a)) {
-        compEnqDeq(b,a,isFIFO,bctx,actx).swap
+      if (isFIFO) {
+        (ctrlValid(a, actx), ctrlValid(b, bctx))
       } else {
-        // a and b on diverging branch off in control hiearchy or are the same controller
-        if (isFIFO) {
-          (ctrlValid(a, actx), ctrlValid(b, bctx))
+        if (a == b) {
+          (ctrlDone(a, actx), ctrlDone(b, bctx))
+        } else if (a.isAncestorOf(b)) {
+          val bAncesstors = (b::b.ancestors)
+          val idx = bAncesstors.indexOf(a)
+          val ctrl = bAncesstors(idx-1).as[ControlTree]
+          (ctrlValid(a, actx), ctrlDone(ctrl, bctx))
+        } else if (b.isAncestorOf(a)) {
+          compEnqDeq(b,a,isFIFO,bctx,actx).swap
         } else {
-          if (a == b) {
-            (ctrlDone(a, actx), ctrlDone(b, bctx))
-          } else {
-            val lca = leastCommonAncesstor(a,b).get
-            val aAncesstors = (a::a.ancestors)
-            val bAncesstors = (b::b.ancestors)
-            val aidx = aAncesstors.indexOf(lca)
-            val bidx = bAncesstors.indexOf(lca)
-            val actrl = aAncesstors(aidx-1).as[ControlTree]
-            val bctrl = bAncesstors(bidx-1).as[ControlTree]
-            (ctrlDone(actrl, actx), ctrlDone(bctrl, bctx))
-          }
+          val lca = leastCommonAncesstor(a,b).get
+          val aAncesstors = (a::a.ancestors)
+          val bAncesstors = (b::b.ancestors)
+          val aidx = aAncesstors.indexOf(lca)
+          val bidx = bAncesstors.indexOf(lca)
+          val actrl = aAncesstors(aidx-1).as[ControlTree]
+          val bctrl = bAncesstors(bidx-1).as[ControlTree]
+          (ctrlDone(actrl, actx), ctrlDone(bctrl, bctx))
         }
       }
     }
