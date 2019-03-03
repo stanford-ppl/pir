5d84ced80719f99dd4a593a10c5471e7c714446e
Yaqi Zhang <hsbzyq@gmail.com>
2019-03-02 22:16:29 -0800

    adding tests

--- regression/OuterProduct.scala
+++ regression/OuterProduct.scala
@@ -6,6 +6,8 @@ case class OuterProductParam(
   ts1:scala.Int = 64,
   ts2:scala.Int = 32,
   ip:scala.Int = 16,
+  ip1:scala.Int = 16,
+  ip2:scala.Int = 1,
   op1:scala.Int = 1,
   op2:scala.Int = 1
 ) extends Param[OuterProductParam]
@@ -15,6 +17,8 @@ class OuterProduct_1 extends OuterProduct {override lazy val param = OuterProduc
 class OuterProduct_2 extends OuterProduct {override lazy val param = OuterProductParam(ip=1, op2=2)}
 class OuterProduct_3 extends OuterProduct {override lazy val param = OuterProductParam(ip=1, op1=2)}
 class OuterProduct_4 extends OuterProduct {override lazy val param = OuterProductParam(ip=1, op1=2, op2=2)}
+class OuterProduct_5 extends OuterProduct {override lazy val param = OuterProductParam(ip=16, op1=2, op2=2)}
+class OuterProduct_6 extends OuterProduct {override lazy val param = OuterProductParam(ip=16, op2=2, ip1=2, ip2=8)}
 //class OuterProduct_2 extends OuterProduct {override lazy val param = OuterProductParam(op2=4)}
 //class OuterProduct_3 extends OuterProduct {override lazy val param = OuterProductParam(op2=6)}
 //class OuterProduct_4 extends OuterProduct {override lazy val param = OuterProductParam(op2=8)}
@@ -53,7 +57,7 @@ class OuterProduct_4 extends OuterProduct {override lazy val param = OuterProduc
           b2 load vec2(j::j+ts2 par ip)
           val outTile = SRAM[T](ts1, ts2)
           // (64, 32)
-          Foreach(ts1 by 1, ts2 par ip){ (ii,jj) => outTile(ii, jj) = b1(ii) * b2(jj) } // 2
+          Foreach(ts1 by ip2, ts2 par ip1){ (ii,jj) => outTile(ii, jj) = b1(ii) * b2(jj) } // 2
           out(i::i+ts1, j::j+ts2 par ip) store outTile
         }
       }
