3a4755248da2e3a1fac304b8e8398ee30c45088e
Yaqi Zhang <hsbzyq@gmail.com>
2019-05-28 20:24:13 -0700

    update NN lib interface

--- regression/streaming/StreamNN.scala
+++ regression/streaming/StreamNN.scala
@@ -19,6 +19,7 @@ class StreamNN_1 extends StreamNN { override lazy val param = StreamNNParam(bp=2
 class StreamNN_2 extends StreamNN { override lazy val param = StreamNNParam(ip=4) }
 class StreamNN_3 extends StreamNN { override lazy val param = StreamNNParam(ip=4, P1=2) }
 class StreamNN_4 extends StreamNN { override lazy val param = StreamNNParam(ip=4, P1=2, bp=2) }
+class StreamNN_5 extends StreamNN { override lazy val param = StreamNNParam(L1=8, ip=8, P1=1, bp=2) }
 
 @spatial trait StreamNN extends StreamTemplate {
 
