d63deb2a233b7936a81c4fc40418d965d9cc8a8a
Yaqi Zhang <hsbzyq@gmail.com>
2019-10-01 16:35:02 -0700

    update dram merge sort

--- regression/SortTest.scala
+++ regression/SortTest.scala
@@ -5,7 +5,7 @@ case class InitSort_1() extends InitSort(ip=16)
 
 @spatial abstract class InitSort(
   N:scala.Int = 64,
-  ts:scala.Int = 32,
+  bs:scala.Int = 32,
   ip:scala.Int = 1
 ) extends SpatialTest {
   type T = Int
@@ -18,11 +18,11 @@ case class InitSort_1() extends InitSort(ip=16)
 
     Accel {
       val mergeBuf = MergeBuffer[T](nway, ip)
-      val mergeSize = ts / nway
-      Sequential.Foreach(N by ts) { i =>
-        val insram = SRAM[T](ts) // Initially in reverse order
-        Foreach(0 until ts) { j =>
-          insram(j) = ts - j
+      val mergeSize = bs / nway
+      Sequential.Foreach(N by bs) { i =>
+        val insram = SRAM[T](bs) // Initially in reverse order
+        Foreach(0 until bs) { j =>
+          insram(j) = bs - 1 - j
         }
         mergeBuf.init(true)
         ways.foreach { w =>
@@ -33,15 +33,15 @@ case class InitSort_1() extends InitSort(ip=16)
             mergeBuf.enq(w, insram(mergeSize*w + j))
           }
         }
-        val fifo = FIFO[T](ts)
-        Foreach(0 until ts) { j =>
+        val fifo = FIFO[T](bs)
+        Foreach(0 until bs) { j =>
           fifo.enq(mergeBuf.deq())
         }
-        dram(i::i+ts par ip) store fifo
+        dram(i::i+bs par ip) store fifo
       }
     }
 
-    val gold = (0 until N by ts) { i => (0 until ts) { i => i } }.flatten
+    val gold = (0 until N by bs) { i => (0 until bs) { i => i } }.flatten
     
     val cksum = checkGold(dram, gold)
     println("PASS: " + cksum + " (InitSort)")
@@ -49,4 +49,75 @@ case class InitSort_1() extends InitSort(ip=16)
   }
 }
 
+case class DRAMMergeSort_0() extends DRAMMergeSort
+case class DRAMMergeSort_1() extends DRAMMergeSort(ip=16)
+case class DRAMMergeSort_2() extends DRAMMergeSort(ip=16, op=2)
 
+@spatial abstract class DRAMMergeSort(
+  N:scala.Int = 1024,
+  initbs:scala.Int = 32, // initial block size. Must be larger than nways * ip
+  op:scala.Int = 1, // Outer loop par
+  ip:scala.Int = 1 // inner loop vectorization
+) extends SpatialTest {
+  type T = Int
+
+  def main(args: Array[String]): Unit = {
+    val nway = 2
+    val ways = scala.List.tabulate(nway) { i => i }
+
+    val in = Seq.tabulate(N) { i => i }.reverse // reverse list
+
+    val dram1 = DRAM[T](N)
+    val dram2 = DRAM[T](N)
+    setMem(dram1, Array.fromSeq(in.map { _.to[T] }))
+    
+    val iters = (math.log(N / initbs) / math.log(nway)).toInt + 1
+
+    Accel {
+      val mergeBuf = MergeBuffer[T](nway, ip)
+      Sequential.Foreach(-1 until iters by 1) { i =>
+        val bs = if (i <= 1) initbs else initbs.to[Int] << (i - 1).to[I16]
+        val mergeSize = bs / nway
+        val first = (i % 2 == 0)
+        val second = !first
+        Sequential.Foreach(N by bs par op) { t =>
+          mergeBuf.init(i < 0)
+          ways.foreach { w =>
+            mergeBuf.bound(w, mergeSize)
+          }
+          val loadfifo1 = FIFO[T](16)
+          val loadfifo2 = FIFO[T](16)
+          if (first) {
+            loadfifo1 alignload dram1(t::t+bs par ip)
+          } else {
+            loadfifo2 alignload dram2(t::t+bs par ip)
+          }
+          ways.foreach { w =>
+            Foreach(0 until mergeSize par ip) { j =>
+              val loaded = mux(first, loadfifo1.deq(first), loadfifo2.deq(second))
+              mergeBuf.enq(w, loaded)
+            }
+          }
+          val storefifo1 = FIFO[T](16)
+          val storefifo2 = FIFO[T](16)
+          Foreach(0 until bs) { j =>
+            val sorted = mergeBuf.deq()
+            storefifo1.enq(sorted, first)
+            storefifo2.enq(sorted, second)
+          }
+          if (first) {
+            dram2(t::t+bs par ip) alignstore storefifo1
+          } else {
+            dram1(t::t+bs par ip) alignstore storefifo2
+          }
+        }
+      }
+    }
+
+    val gold = Array.tabulate(N) { i => i.to[T] }
+
+    val output = IfElse(iters % 2 == 0) { dram1 } { dram2 } // Unstaged if statement
+    
+    assert(checkGold(output, gold))
+  }
+}
