7211fd9c9ae89d672855cf6ade2275f609869857
Nathan Zhang <nzhang32@gmail.com>
2019-10-09 13:30:15 -0700

    working full gurobi based solver

--- bin/partition.py
+++ bin/partition.py
@@ -10,6 +10,7 @@ import cvxpy
 from collections import namedtuple, defaultdict
 import operator
 import functools
+import typing
 
 Constraint = namedtuple("Constraint", ["ops", "vin", "sin", "vout", "sout"])
 Node = namedtuple("Node", ["node", "op", "comment"])
@@ -27,88 +28,141 @@ def cleaned_name(node_id):
     return node_id
 
 
-def partition(nodes, edges, constraints: Constraint):
-    """
-    :param nodes: List of Node objects
-    :param edges: List of Edge objects
-    :param constraints: Constraint
-    :return: Mapping[int, List[Nodes]] a mapping from partition ID to nodes in partition
-    """
-    # using CVX
-    num_nodes = len(nodes)
-    partitions = num_nodes
-    node_to_loc_map = {node_id.node: i for i, node_id in enumerate(nodes)}
-    node_to_partition_map = cvxpy.Variable(name="Mapping", shape=(num_nodes, partitions), boolean=True)
-    node_to_partition_map.value = np.fliplr(np.identity(num_nodes))
+class CVXPartitioner:
+    nodes: typing.List[Node]
+    edges: typing.List[Edge]
+    constraints: Constraint
+    cvx_constraints: typing.List
 
-    node_partitions = cvxpy.Variable(name="Partitions", shape=(num_nodes,), pos=True)
-    partition_eq_constraint = node_partitions == node_to_partition_map @ np.arange(partitions)
+    def __init__(self, nodes, edges, constraints):
+        self.nodes = nodes[:]
+        self.edges = edges[:]
+        self.constraints = constraints
+        self.cvx_constraints = []
 
-    stochasticity_constraint = cvxpy.sum(node_to_partition_map, axis=1) == 1
-    op_constraint = cvxpy.sum(node_to_partition_map, axis=0) <= constraints.ops
+        self.num_nodes = len(nodes)
+        self.partitions = self.num_nodes
+        self.node_to_loc_map = {node_id.node: i for i, node_id in enumerate(nodes)}
+        self.node_to_partition_matrix = cvxpy.Variable(name="Matrix", shape=(self.num_nodes, self.partitions),
+                                                       boolean=True,
+                                                       value=np.fliplr(np.identity(self.num_nodes)))
 
-    weights = np.arange(partitions)
-    filtered_edges = [edge for edge in edges if edge.src in node_to_loc_map and edge.dst in node_to_loc_map]
-    dep_constraint_matrix = scipy.sparse.lil_matrix((len(filtered_edges), num_nodes))
-    for i, (src, dst, _, _) in enumerate(filtered_edges):
-        dep_constraint_matrix[i, node_to_loc_map[src]] = -1
-        dep_constraint_matrix[i, node_to_loc_map[dst]] = 1
+        # constructs an array of just the partition numbers. Useful for dep constraints.
+        self.node_partitions = cvxpy.Variable(name="Partitions", shape=(self.num_nodes,), nonneg=True)
+        self.cvx_constraints.append(self.node_partitions == self.node_to_partition_matrix @ np.arange(self.partitions))
 
-    dep_constraint = (dep_constraint_matrix.tocsr() * node_to_partition_map * weights) >= 0
+        # stochasticity constraint: each node only maps to 1 partition.
+        self.cvx_constraints.append(cvxpy.sum(self.node_to_partition_matrix, axis=1) == 1)
 
-    # in/out degree constraints
-    output_limits = {
-        "s": constraints.sout,
-        "v": constraints.vout
-    }
+        # constraint on number of ops
+        total_ops = functools.reduce(operator.add, [
+            self.node_to_partition_matrix[self.node_to_loc_map[node.node], :] * node.op for node in self.nodes])
+        self.cvx_constraints.append(total_ops <= self.constraints.ops)
+        self._init_dep_constraints()
+        self._init_input_constraints()
 
-    # these nodes always have an output edge.
-    nodes_with_external_outs = {(edge.src, edge.tp) for edge in edges if
-                                edge.src in node_to_loc_map and edge.dst not in node_to_loc_map}
-    always_out_mask = {
-        "s": np.zeros(num_nodes),
-        "v": np.zeros(num_nodes)
-    }
-    for node, tp in nodes_with_external_outs:
-        always_out_mask[tp][node_to_loc_map[node]] = 1
+    def _init_dep_constraints(self):
+        filtered_edges = [edge for edge in self.edges if
+                          edge.src in self.node_to_loc_map and edge.dst in self.node_to_loc_map]
+        dep_constraint_matrix = scipy.sparse.lil_matrix((len(filtered_edges), self.num_nodes))
+        for src, dst, _, _ in filtered_edges:
+            self.cvx_constraints.append(
+                self.node_partitions[self.node_to_loc_map[src]] <= self.node_partitions[self.node_to_loc_map[dst]])
 
-    outputs = {}
-    for k, mask in always_out_mask.items():
-        outputs[k] = mask @ node_to_partition_map
+    def _init_input_constraints(self):
+        input_constraints = {
+            "s": self.constraints.sin,
+            "v": self.constraints.vin
+        }
+        node_push_map = {
+            "s": defaultdict(set),
+            "v": defaultdict(set)
+        }
 
-    node_outs = {
-        "s" : defaultdict(set),
-        "v" : defaultdict(set)
-    }
-    for src, dst, tp, _ in edges:
-        if (src, tp) in nodes_with_external_outs:
-            continue
-        if src not in node_to_loc_map:
-            continue
-        node_outs[tp][node_to_loc_map[src]].add(node_to_loc_map[dst])
+        for src, dst, tp, _ in self.edges:
+            if dst not in self.node_to_loc_map:
+                continue
+            node_push_map[tp][src].add(dst)
 
-    approx_constraints = []
-    for tp, node_to_out_map in node_outs.items():
-        for src, destinations in node_to_out_map.items():
-            diff = node_partitions[list(destinations)] - node_partitions[src]
-            # diff[i] == 0 means they're on the same partition
-            # output_ind = cvxpy.minimum(cvxpy.sum(diff), 1)
-            output_ind = cvxpy.max(diff)
-            # output_ind != 0 means diff[i] != 0
-            # if diff == 0, then this gives 0.
-            # if diff == 1, then it's 0 everywhere except for the right location
-            outputs[tp] += cvxpy.maximum(output_ind + node_to_partition_map[src, :] - 1, 0)
+        for tp, push_map in node_push_map.items():
+            movement = []
+            for src, destinations in push_map.items():
+                if src not in self.node_to_loc_map:
+                    # we use this to filter out all of the contributions within a partition.
+                    # since external nodes aren't in any relevant partition, they always contribute.
+                    base_filter = np.zeros(self.partitions)
+                else:
+                    base_filter = self.node_to_partition_matrix[self.node_to_loc_map[src], :]
 
-    output_constraints = [outputs[tp] <= output_limits[tp] for tp in "sv"]
-    problem = cvxpy.Problem(cvxpy.Minimize(cvxpy.sum(node_to_partition_map * np.arange(partitions))), [
-        stochasticity_constraint,
-        op_constraint,
-        dep_constraint,
-        partition_eq_constraint
-    ] + output_constraints + approx_constraints)
-    problem.solve(solver="GUROBI", verbose=True, warm_start=True, Threads=64)
-    print("SOLVED")
-    print(node_partitions.value)
+                contributions = functools.reduce(operator.add, [
+                    self.node_to_partition_matrix[self.node_to_loc_map[dst]] for dst in destinations])
+                movement.append(cvxpy.maximum(self._project_to_bool(contributions) - base_filter, 0))
+
+            self.cvx_constraints.append(
+                functools.reduce(operator.add, movement) <= input_constraints[tp])
+
+    def _init_output_constraints(self):
+        output_constraints = {
+            "s": self.constraints.sout,
+            "v": self.constraints.vout
+        }
+        # for each node, if it's used anywhere not in the same partition, then it counts.
+
+        # an external output automatically counts
+        nodes_with_external_outputs = {
+            "s": set(),
+            "v": set()
+        }
+        for src, dst, tp, _ in self.edges:
+            if src in self.node_to_loc_map and dst not in self.node_to_loc_map:
+                nodes_with_external_outputs[tp].add(src)
+
+        node_push_map = {
+            "s": defaultdict(set),
+            "v": defaultdict(set)
+        }
+
+        for src, dst, tp, _ in self.edges:
+            # don't count external nodes
+            if src not in self.node_to_loc_map:
+                continue
+            # if a node already pushes out to an external node, we know it's guaranteed to have an output.
+            if src in nodes_with_external_outputs[tp]:
+                continue
+
+            node_push_map[tp][self.node_to_loc_map[src]].add(self.node_to_loc_map[dst])
+
+        for tp, push_map in node_push_map.items():
+            exports = []
+            for src, destinations in push_map.items():
+                local_exports = functools.reduce(operator.add, [
+                    self.node_to_partition_matrix[dst, :] for dst in destinations
+                ])
+                exports.append(cvxpy.maximum(self._project_to_bool(local_exports) - self.node_to_partition_matrix[src, :], 0))
+
+            total_movement = functools.reduce(operator.add, exports)
+            self.cvx_constraints.append(total_movement <= output_constraints[tp])
+
+
+    def _objective(self):
+        return cvxpy.Minimize(cvxpy.max(self.node_partitions))
+
+    def _project_to_bool(self, var):
+        projected = cvxpy.Variable(boolean=True, shape=var.shape)
+        slack = cvxpy.Variable(shape=var.shape, nonneg=True)
+        self.cvx_constraints.append(projected + slack >= var)
+        self.cvx_constraints.append(slack <= projected * self.partitions * self.num_nodes)
+        return projected
+
+    def solve(self, **kwargs):
+        problem = cvxpy.Problem(self._objective(), self.cvx_constraints)
+        problem.solve(**kwargs)
+
+    def get_assignment(self):
+        return [
+            (node.node, int(self.node_partitions.value[self.node_to_loc_map[node.node]]))
+            for node in self.nodes
+        ]
 
 
 def main():
@@ -132,8 +186,14 @@ def main():
     constraint = load_csv(opts.spec, Constraint)[0]
     constraint = Constraint(*list(map(int, constraint)))
 
-    partition(nodes, edges, constraint)
+    # partition(nodes, edges, constraint)
+    solver = CVXPartitioner(nodes, edges, constraint)
+    solver.solve(solver="GUROBI", verbose=True, warm_start=True, Threads=64, MIPGap=0.1)
 
+    with open(opts.partition, "w") as pf:
+        writer = csv.writer(pf, delimiter=",")
+        for node, partition in solver.get_assignment():
+            writer.writerow([node, partition])
 
 if __name__ == "__main__":
     main()
