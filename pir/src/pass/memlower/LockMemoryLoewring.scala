0b2ed935934feaed5eb6383548f96d1e6b07e479
Yaqi Zhang <hsbzyq@gmail.com>
2019-10-23 17:18:21 -0700

    adapt unified barrier insertion to lock mem lowering

--- pir/src/pass/memlower/LockMemoryLoewring.scala
+++ pir/src/pass/memlower/LockMemoryLoewring.scala
@@ -18,20 +18,33 @@ trait LockMemoryLowering extends GenericMemoryLowering {
   protected def lowerUnparLockMem(n:Memory) = dbgblk(s"lowerUnparLockMem($n)"){
     val memCU = within(pirTop) { MemoryContainer() }
     swapParent(n, memCU)
-    val lockMap = n.accesses.groupBy { access => access.as[LockAccess].lock.T }
+    val accesses = n.accesses.as[List[LockAccess]]
+    val lockMap = accesses.groupBy { access => access.lock.T }
     val lastAccesses = lockMap.flatMap {
       case (None, accesses) => None
       case (Some(lock), accesses) => Some(accesses.maxBy { _.order.get })
     }.toSet
     dbg(s"lastAccesses=${lastAccesses.mkString(",")}")
-    n.accesses.foreach { access =>
-      lowerAccess(n, memCU, access.as[LockAccess], lastAccesses.contains(access))
+    withLive(accesses:_*) {
+      accesses.foreach { access =>
+        lowerAccess(n, memCU, access, lastAccesses.contains(access))
+      }
+      val syncAccesses = accesses.groupBy { _.getCtrl }.flatMap { case (ctrl, accesses) =>
+        val (locked, unlocked) = accesses.partition { _.lock.isConnected }
+        unlocked ++ locked.headOption
+      }.toList
+      consistencyBarrier(syncAccesses) { case (deped, dep) =>
+        val carried = dep.progorder.get > deped.progorder.get
+        !carried
+      }{ case (from,to,carried) =>
+        insertToken(from.ctx.get, to.ctx.get).depth(1)
+      }
+      accesses.foreach { access =>
+        val ctx = access.ctx.get
+        bufferInput(ctx, fromCtx=addrCtxs.get(access.getCtrl))
+      }
     }
-    sequencedScheduleBarrierInsertion(n)
-    n.accesses.foreach { access =>
-      val ctx = access.ctx.get
-      bufferInput(ctx, fromCtx=addrCtxs.get(access.getCtrl))
-    }
+    free(accesses)
     addrCtxs.clear
   }
 
