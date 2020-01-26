89db11288b4030ef77583573a09b7fcf02866239
Yaqi Zhang <hsbzyq@gmail.com>
2020-01-26 15:55:04 -0800

    update script

--- bin/spatial_test.py
+++ bin/spatial_test.py
@@ -1,22 +1,26 @@
 import os
 import subprocess
 from util import *
+import socket
 
 def main(args=None):
+    parser = argparse.ArgumentParser(description='Run experiments')
+    parser.add_argument('-a', '--app', action='append', help='name of application to run')
+    parser.add_argument('-b', '--backend', type=str, action="append", help='Testing Backend')
+    parser.add_argument('-t', '--thread', type=int, default=16, help='Number of threads to run')
+    parser.add_argument('-p', '--project', type=str, default="pirTest", help='project name')
+    parser.add_argument('-r', '--rerun', action='append', help='passes to rerun', default=[])
     parser.add_argument('-f', '--cppfast', action='store_true', default=False)
     parser.add_argument('-H', '--hybrid', action='store_true', default=False)
     parser.add_argument('-T', '--tee', action='store_true', default=False)
     parser.add_argument('-u', '--publish', action='store_true', default=False)
     (opts, args) = parser.parse_known_args(args=args)
 
-    spatial_flags = [arg for arg in args if '--s:' in arg]
-    args = [arg for arg in args if '--s:' not in arg]
-    spatial_flags = [arg.replace('--s:','--') for arg in spatial_flags]
-
     if opts.publish:
         cp = subprocess.run("sbt publishAll", shell=True, cwd='pir/')
         cp.check_returncode()
 
+    # resolve apps to run
     if opts.app is None:
         opts.app = ["*"]
     apps = []
@@ -28,14 +32,33 @@ def main(args=None):
         else: 
             apps.append(app)
     opts.app = apps
-    java_cmd = ""
+
+    # resolve flags and args
+    if opts.backend is None:
+        spatialargs = args
+        pirargs = []
+    else:
+        spatialargs = [arg for arg in args if '--s:' in arg]
+        pirargs = [arg for arg in args if '--s:' not in arg]
+        spatialargs = [arg.replace('--s:','--') for arg in spatialargs]
+
     d = get_configs()
-    enablePIR = '--pir' in args or (opts.backend is not None and 'Tst' in opts.backend)
-    if 'spatial-home' not in d and enablePIR:
-        args.insert(0,f'--spatial-home={os.getcwd()}')
-    java_cmd += "export TEST_ARGS=\"{}\"; ".format(' '.join(args))
-    if len(spatial_flags) != 0:
-        java_cmd += "export SPATIAL_FLAGS=\"{}\"; ".format(' '.join(spatial_flags))
+    if 'spatial-home' not in d and opts.backend is not None:
+        pirargs.insert(0,f'--spatial-home={os.getcwd()}')
+    hostname = socket.gethostname()
+
+    # if on cluster and currently under /home, put gen in the scratch
+    if all(['--gendir' not in arg for arg in args]) and \
+            (hostname in ['lagos','tucson'] or 'edo-' in hostname) and \
+            os.getcwd().startswith('/home'):
+        current = os.getcwd()
+        scratch = current.replace('/home/','/scratch/') + '/gen'
+        print(f'Change gendir to {scratch}. Use --gendir override')
+        spatialargs.append('--gendir={}'.format(scratch))
+
+    java_cmd = ""
+    java_cmd += "export TEST_ARGS=\"{}\"; ".format(' '.join(spatialargs))
+    java_cmd += "export PIR_ARGS=\"{}\"; ".format(' '.join(pirargs))
     java_cmd += "sbt -Dmaxthreads={} ".format(opts.thread)
     if (opts.backend is not None):
         for b in opts.backend:
