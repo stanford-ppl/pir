c3832f82a1d3a53cf2f6c952fd9fe6a5b6bb7918
Yaqi Zhang <hsbzyq@gmail.com>
2020-02-19 00:19:44 -0800

    add comment in app

--- regression/SimpleSparse.scala
+++ regression/SimpleSparse.scala
@@ -5,6 +5,17 @@ import spatial.dsl._
   //type T = FixPt[TRUE, _16, _16]
   type T = Int
 
+  // init in barrier is the number of initial tokens in the barrier buffers. If set to x, the accesses reading
+  // (popping) the barrier can proceed x times before waiting on the writer (pusher). 
+  // The location where the barrier is declared determines when the token is pushed/popped
+  // In this example the forward barrier force the read to wait for write every ts iterations.
+  // The backwardBarrier forces the write of i+1 iteration to wait on read of previous
+  // iteration to prevent overriding the data before they get read. SparseSRAMs are not
+  // multi-buffered so init=1.
+  // If the barrier is declared outside the loop N by ts, the read waits the writer runs finishes
+  // the entire (N by ts) and (ts by 1) loop before proceed, which in this case produce incorrect
+  // result.
+  
   def main(args: Array[String]): Unit = {
 
     val N = 128
@@ -18,7 +29,7 @@ import spatial.dsl._
       val s1 = SparseSRAM[T](ts)
       Reduce(out)(N by ts) { i =>
         val forwardBarrier = Barrier[Token](0)
-        val backwardBarrier = Barrier[Token](1) // Always 1 because sparse memory is not multi-buffered
+        val backwardBarrier = Barrier[Token](init=1) 
         Foreach(ts by 1 par ip) { j =>
           s1.barrierWrite(j, i+j, Seq(forwardBarrier.push, backwardBarrier.pop))
         }
