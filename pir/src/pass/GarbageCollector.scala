f9654f20dff9945f7f6fb32e97033253297ef24a
Alex Rucker <acrucker@lagos.stanford.edu>
2020-07-09 19:10:13 -0700

    Fix bug in GC logic.

--- pir/src/pass/GarbageCollector.scala
+++ pir/src/pass/GarbageCollector.scala
@@ -125,7 +125,8 @@ trait GarbageCollector { self:PIRTransformer =>
     case n:HostOutController => Some(true)
     case n:Controller if !depDupHasRun => Some(true)
     case n:AccumAck if !dramBarrierInsertionHasRun => Some(true)
-    case n@SparseMem("SRAM",_) if n.alias.v.nonEmpty => Some(true)
+    case n@SparseMem("ParDRAM",_) if n.alias.v.nonEmpty => Some(true)
+    case n@SparseMem("ParSRAM",_) if n.alias.v.nonEmpty => Some(true)
     case n:SparseAccess if n.mem.isConnected && n.mem.T.as[SparseMem].memType == "SRAM" => Some(true) // All sparse SRAM
     case n if n.isUnder[Controller] && !depDupHasRun => Some(true)
     case n if states.liveNodes.contains(n) => Some(true)
