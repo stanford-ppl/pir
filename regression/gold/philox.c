a646c224cd4e0eca90f0332002eab3372fb43dee
Paul Mure <paulmure2000@gmail.com>
2021-10-28 21:31:45 -0700

    improve c implementation

--- regression/gold/philox.c
+++ regression/gold/philox.c
@@ -1,97 +1,79 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define NUM_ROUNDS 7
-#define MULT_0 0xCD9E8D57
-#define MULT_1 0xD2511F53
-#define WEYL_0 0x7F4A7C15
-#define WEYL_1 0x84CAA73B
+#define MULT_0 0xD2511F53
+#define MULT_1 0xCD9E8D57
+#define WEYL_0 0x9E3779B9
+#define WEYL_1 0xBB67AE85
 
-typedef struct feistel {
-    uint32_t L;
-    uint32_t R;
-    uint32_t key;
-    uint32_t const M;
-    uint32_t const weyl;
-} feistel_t;
+void mulhilo(uint32_t a, uint32_t b, uint32_t *hi, uint32_t *lo) {
+    uint64_t mul = ((uint64_t) a) * ((uint64_t) b);
+    *lo = (uint32_t)mul;
+    *hi = (uint32_t)(mul >> 32);
+}
 
-static feistel_t philox_states[2] = {
-    { .L = 0, .R = 0, .key = 0, .M = MULT_0, .weyl = WEYL_0 },
-    { .L = 0, .R = 0, .key = 0, .M = MULT_1, .weyl = WEYL_1 },
-};
+void single_round(uint32_t ctr[4], const uint32_t key[2]) {
+    uint32_t lo0, hi0, lo1, hi1;
+    mulhilo(ctr[0], MULT_0, &hi0, &lo0);
+    mulhilo(ctr[2], MULT_1, &hi1, &lo1);
+    ctr[0] = hi1 ^ key[0] ^ ctr[1];
+    ctr[1] = lo1;
+    ctr[2] = hi0 ^ key[1] ^ ctr[3];
+    ctr[3] = lo0;
+}
 
-void print_state() {
-    for (int i = 0; i < 2; ++i) {
-        printf("State[%d], { .L = %u, .R = %u, .key = %u, .M = %u, .weyl = %u}\n",
-                i+1,
-                philox_states[0].L,
-                philox_states[0].R,
-                philox_states[0].key,
-                philox_states[0].M,
-                philox_states[0].weyl);
+void philox_compute(const uint32_t ctr[4], const uint32_t key[2], uint32_t out[4]) {
+    uint32_t ctr_[4], key_[2];
+    memcpy(ctr_, ctr, sizeof(ctr_));
+    memcpy(key_, key, sizeof(key_));
+
+    for (size_t i = 0; i < NUM_ROUNDS - 1; ++i) {
+        single_round(ctr_, key_);
+        key_[0] += WEYL_0; key_[1] += WEYL_1;
     }
-    printf("\n");
+    single_round(ctr_, key_);
+
+    memcpy(out, ctr_, sizeof(ctr_));
 }
 
-void sbox(feistel_t *state) {
-    uint64_t prod = ((uint64_t)state->R) * ((uint64_t)state->M);
-    uint32_t LL = (uint32_t)prod;
-    uint32_t RR = (uint32_t)(prod >> 32) ^ state->L ^ state->key;
-    state->L = LL;
-    state->R = RR;
-    state->key += state->weyl;
+void incr(uint32_t ctr[4]) {
+    // series of checking overflow
+    if (++ctr[0]) return;
+    if (++ctr[1]) return;
+    if (++ctr[2]) return;
+    ++ctr[3];
 }
 
-void swap(uint32_t *a, uint32_t *b) {
-    // perhaps Plasticine can benefit from XOR swap?
-    uint32_t tmp = *a;
-    *a = *b;
-    *b = tmp;
-}
-
-void pbox(feistel_t (*states)[2]) {
-    swap(&states[0]->R, &states[1]->R);
-}
-
-void set_key(uint32_t keys[2]) {
-    philox_states[0].key = keys[0];
-    philox_states[1].key = keys[1];
-}
-
-void set_counter(uint32_t counter[4]) {
-    philox_states[0].L = counter[0];
-    philox_states[0].R = counter[1];
-    philox_states[1].L = counter[2];
-    philox_states[1].R = counter[3];
-}
-
-uint32_t philox() {
-    for (size_t i = 0; i < NUM_ROUNDS; ++i) {
-        pbox(&philox_states);
-        sbox(&philox_states[0]);
-        sbox(&philox_states[1]);
-    }
-    return philox_states[0].L;
+uint32_t philox(uint32_t ctr[4], const uint32_t key[2], unsigned *selector) {
+    uint32_t out[4];
+    philox_compute(ctr, key, out);
+    incr(ctr);
+    uint32_t res = out[*selector];
+    *selector = (*selector + 1) & 3;
+    return res;
 }
 
 int main(int argc, char *argv[]) {
     if (argc != 7) {
-        printf("not enough arguments");
+        printf("not enough arguments\n");
         return 1;
     }
-    uint32_t keys[2];
-    uint32_t counter[4];
-    keys[0] = strtoul(argv[1], NULL, 10);
-    keys[1] = strtoul(argv[2], NULL, 10);
-    counter[0] = strtoul(argv[3], NULL, 10);
-    counter[1] = strtoul(argv[4], NULL, 10);
-    counter[2] = strtoul(argv[5], NULL, 10);
-    counter[3] = strtoul(argv[6], NULL, 10);
-    set_key(keys);
-    set_counter(counter);
+    uint32_t key[2];
+    uint32_t ctr[4];
+    key[0] = strtoul(argv[1], NULL, 10);
+    key[1] = strtoul(argv[2], NULL, 10);
+    ctr[0] = strtoul(argv[3], NULL, 10);
+    ctr[1] = strtoul(argv[4], NULL, 10);
+    ctr[2] = strtoul(argv[5], NULL, 10);
+    ctr[3] = strtoul(argv[6], NULL, 10);
+    unsigned selector = 0;
 
-    for (size_t i = 0; i < 10; ++i) {
-        printf("rand = %u\n", philox());
+    for (unsigned i = 0; i < 10; ++i) {
+        printf("%u\n", philox(ctr, key, &selector));
     }
+
+    return 0;
 }
