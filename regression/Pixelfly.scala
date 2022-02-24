07e2a100fb251738b36548eea87117c867b31987
Sho Ko <kosho@lagos.stanford.edu>
2022-02-23 20:54:26 -0800

    new

--- regression/Pixelfly.scala
+++ regression/Pixelfly.scala
@@ -6,14 +6,15 @@ import spatial.lib.metaprogramming._
 import java.io._
 import utils.io.files._
 
-class Pixelfly_16_2 extends Pixelfly(
+class Pixelfly_16_2_128 extends Pixelfly( // Pixelfly_N_B_batch
     N = 16,
     B = 2,
     V = 4,
     data = "/home/kosho/data/16_2",
     s_list = List(16, 8, 4, 2),
     s_over_2_list = List(8, 4, 2, 1),
-    N_over_s_list = List(1, 2, 4, 8)
+    N_over_s_list = List(1, 2, 4, 8),
+    batch = 128
 )
 
 @spatial abstract class Pixelfly(
@@ -24,6 +25,7 @@ class Pixelfly_16_2 extends Pixelfly(
     s_list: scala.List[scala.Int] = List(16, 8, 4, 2),
     s_over_2_list: scala.List[scala.Int] = List(8, 4, 2, 1),
     N_over_s_list: scala.List[scala.Int] = List(1, 2, 4, 8),
+    batch: scala.Int = 128,
     iteration: scala.Int = 2,
     
     ip: scala.Int = 16
@@ -33,24 +35,31 @@ class Pixelfly_16_2 extends Pixelfly(
     type T = Float
     
     def main(args: Array[String]): Unit = {
-        // val w = DRAM[T](V*N*B*2*B)
-        // setMem(w, loadCSV1D[T](data + "/w.csv"))
+        val w = DRAM[T](V*N*B*2*B)
+        setMem(w, loadCSV1D[T](data + "/w.csv"))
         
-        // val in = DRAM[T](N*B)
-        // setMem(in, loadCSV1D[T](data + "/in.csv"))
+        val in = DRAM[T](batch*N*B)
+        setMem(in, loadCSV1D[T](data + "/in.csv"))
         
-        // val out = DRAM[T](N*B)
+        val out = DRAM[T](batch*N*B)
         
         Accel {
-            val w_sram = SRAM[T](V*N*B*2*B)
-            val in_sram = SRAM[T](N*B)
-            val out_sram = SRAM[T](N*B)   
-            
-            // w_sram load w(0::V*N*B*2*B par ip)
-            // in_sram load in(0::N*B par ip)
+            val w_sram = SRAM[T](V*N*B*2*B)    
+            w_sram load w(0::V*N*B*2*B par ip)
             
             
-            Foreach(0 until iteration by 1) { iter =>
+            
+            
+            
+            
+            Foreach(0 until batch by 1) { ba =>
+            
+                val in_sram = SRAM[T](N*B)
+                val out_sram = SRAM[T](N*B)
+            
+                
+                in_sram load in(ba*N*B::(ba+1)*N*B par ip) 
+                
                 val tmp_out_sram = (0 to V-1) map {i => SRAM[T](N*B)} 
                 
                 for (v <- 0 to V-1) {
@@ -72,16 +81,20 @@ class Pixelfly_16_2 extends Pixelfly(
                 
                 Foreach(0 until N*B by 1) { i =>
                     out_sram(i) = tmp_out_sram map {a => a(i)} reduceTree{_+_} 
-                }
+                }           
                 
+                out(ba*N*B::(ba+1)*N*B par ip) store out_sram  
             }
             
           
+          
+          
+          
             
-            // out(0::N*B par ip) store out_sram
+            
         }
         
-        // writeCSV1D(getMem(out), data + "/out.csv")
+        writeCSV1D(getMem(out), data + "/out.csv")
         
         assert(true)
     }
