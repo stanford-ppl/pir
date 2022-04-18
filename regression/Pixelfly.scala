45f6d42d1e06a9237456e391f193eb3bce0f650d
Sho Ko <kosho@lagos.stanford.edu>
2022-04-17 23:11:39 -0700

    new

--- regression/Pixelfly.scala
+++ regression/Pixelfly.scala
@@ -10,7 +10,18 @@ class Pixelfly_16_2_128 extends Pixelfly( // Pixelfly_N_B_batch
     N = 16,
     B = 2,
     V = 4,
-    data = "/home/kosho/data/16_2_128",
+    data = "/home/kosho/data/16_2_128_test",
+    s_list = List(16, 8, 4, 2),
+    s_over_2_list = List(8, 4, 2, 1),
+    N_over_s_list = List(1, 2, 4, 8),
+    batch = 128
+)
+
+class Pixelfly_16_4_128 extends Pixelfly( // Pixelfly_N_B_batch
+    N = 16,
+    B = 4,
+    V = 4,
+    data = "/home/kosho/data/16_4_128_test",
     s_list = List(16, 8, 4, 2),
     s_over_2_list = List(8, 4, 2, 1),
     N_over_s_list = List(1, 2, 4, 8),
@@ -21,65 +32,21 @@ class Pixelfly_32_2_128 extends Pixelfly( // Pixelfly_N_B_batch
     N = 32,
     B = 2,
     V = 5,
-    data = "/home/kosho/data/32_2_128",
+    data = "/home/kosho/data/32_2_128_test",
     s_list = List(32, 16, 8, 4, 2),
     s_over_2_list = List(16, 8, 4, 2, 1),
     N_over_s_list = List(1, 2, 4, 8, 16),
     batch = 128
 )
 
-class Pixelfly_64_2_128 extends Pixelfly( // Pixelfly_N_B_batch
-    N = 64,
-    B = 2,
-    V = 6,
-    data = "/home/kosho/data/64_2_128",
-    s_list = List(64, 32, 16, 8, 4, 2),
-    s_over_2_list = List(32, 16, 8, 4, 2, 1),
-    N_over_s_list = List(1, 2, 4, 8, 16, 32),
-    batch = 128
-)
-
-class Pixelfly_128_2_128 extends Pixelfly( // Pixelfly_N_B_batch
-    N = 128,
-    B = 2,
-    V = 7,
-    data = "/home/kosho/data/128_2_128",
-    s_list = List(128, 64, 32, 16, 8, 4, 2),
-    s_over_2_list = List(64, 32, 16, 8, 4, 2, 1),
-    N_over_s_list = List(1, 2, 4, 8, 16, 32, 64),
-    batch = 128
-)
-
-class Pixelfly_256_2_128 extends Pixelfly( // Pixelfly_N_B_batch
-    N = 256,
-    B = 2,
-    V = 8,
-    data = "/home/kosho/data/256_2_128",
-    s_list = List(256, 128, 64, 32, 16, 8, 4, 2),
-    s_over_2_list = List(128, 64, 32, 16, 8, 4, 2, 1),
-    N_over_s_list = List(1, 2, 4, 8, 16, 32, 64, 128),
-    batch = 128
-)
-
-class Pixelfly_512_2_128 extends Pixelfly( // Pixelfly_N_B_batch
-    N = 512,
-    B = 2,
-    V = 9,
-    data = "/home/kosho/data/512_2_128",
-    s_list = List(512, 256, 128, 64, 32, 16, 8, 4, 2),
-    s_over_2_list = List(256, 128, 64, 32, 16, 8, 4, 2, 1),
-    N_over_s_list = List(1, 2, 4, 8, 16, 32, 64, 128, 256),
-    batch = 128
-)
-
-class Pixelfly_1024_2_128 extends Pixelfly( // Pixelfly_N_B_batch
-    N = 1024,
-    B = 2,
-    V = 10,
-    data = "/home/kosho/data/1024_2_128",
-    s_list = List(1024, 512, 256, 128, 64, 32, 16, 8, 4, 2),
-    s_over_2_list = List(512, 256, 128, 64, 32, 16, 8, 4, 2, 1),
-    N_over_s_list = List(1, 2, 4, 8, 16, 32, 64, 128, 256, 512),
+class Pixelfly_32_4_128 extends Pixelfly( // Pixelfly_N_B_batch
+    N = 32,
+    B = 4,
+    V = 5,
+    data = "/home/kosho/data/32_4_128_test",
+    s_list = List(32, 16, 8, 4, 2),
+    s_over_2_list = List(16, 8, 4, 2, 1),
+    N_over_s_list = List(1, 2, 4, 8, 16),
     batch = 128
 )
 
@@ -101,40 +68,41 @@ class Pixelfly_1024_2_128 extends Pixelfly( // Pixelfly_N_B_batch
     type T = Float
     
     def main(args: Array[String]): Unit = {
-        val w = DRAM[T](V*N*B*2*B)
-        setMem(w, loadCSV1D[T](data+"/w.csv"))
+        val w = (0 to V-1) map {v => DRAM[T](N_over_s_list(v), s_over_2_list(v), 2, B, 2, B)}
+        for (v <- 0 to V-1) {
+            setMem(w(v), loadCSV1D[T](data+"/w_"+v.toString+".csv"))
+        }
         
-        val in = DRAM[T](batch*N*B)
+        val in = DRAM[T](batch, N*B)
         setMem(in, loadCSV1D[T](data+"/in.csv"))
         
-        val out = DRAM[T](batch*N*B)
+        val out = DRAM[T](batch, N*B)
         
-        val err_in = DRAM[T](batch*N*B)
-        val w_modified = DRAM[T](V*N*B*2*B)
+        val err_in = DRAM[T](batch, N*B)
         
         Accel {
-            val w_sram = (0 to V-1) map {i => SRAM[T](N*B*2*B).buffer} // 15 MU
+            val w_sram = (0 to V-1) map {v => SRAM[T](N_over_s_list(v), s_over_2_list(v), 2, B, 2, B).buffer.hierarchical}
             
-            for (v <- 0 to V-1) {    
-                w_sram(v) load w(v*N*B*2*B::(v+1)*N*B*2*B par ip) // 20 CU
+            for (v <- 0 to V-1) {
+                w_sram(v) load w(v)(0::N_over_s_list(v), 0::s_over_2_list(v), 0::2, 0::B, 0::2 par 2, 0::B par B)
             }
             
             
             Foreach(0 until batch) { ba =>
             
                 val in_sram = SRAM[T](N*B).buffer // 14 MU
-                in_sram load in(ba*N*B::(ba+1)*N*B par ip) // 16 CU 
+                in_sram load in(ba, 0::N*B par ip) // 16 CU
                 
                 
                  
                 val tmp_out_sram = (0 to V-1) map {i => SRAM[T](N*B).buffer} // 5 MU
                 val out_sram = SRAM[T](N*B).buffer // 6 MU
                 
-                val w_tmp_sram = (0 to V-1) map {i => SRAM[T](N*B*2*B).buffer} // 6 MU
+                val w_tmp_sram = (0 to V-1) map {v => SRAM[T](N_over_s_list(v), s_over_2_list(v), 2, B, 2, B).buffer.hierarchical} // 6 MU
                 val tmp_err_in_sram = (0 to V-1) map {i => SRAM[T](N*B).buffer} // 5 MU
                 val err_in_sram = SRAM[T](N*B).buffer // 1 MU
                 
-                val w_new_sram = (0 to V-1) map {i => SRAM[T](N*B*2*B).buffer} // 5 MU
+                val w_new_sram = (0 to V-1) map {v => SRAM[T](N_over_s_list(v), s_over_2_list(v), 2, B, 2, B).buffer.hierarchical} // 5 MU
                 
                 
                 
@@ -146,11 +114,11 @@ class Pixelfly_1024_2_128 extends Pixelfly( // Pixelfly_N_B_batch
                     val s_over_2 = s_over_2_list(v)
                     val N_over_s = N_over_s_list(v)
                     
-                    Foreach(0 until N_over_s, 0 until 2, 0 until s_over_2, 0 until B) { case Seq(c, tv, d, bv) =>
+                    Foreach(0 until N_over_s, 0 until s_over_2, 0 until 2, 0 until B) { case Seq(c, d, tv, bv) =>
                         val sum = Reduce(Reg[T])(0 until 2 par 2, 0 until B par B) { (th, bh) =>
-                            w_sram(v)(c*2*s*B*B + tv*s*B*B + d*B*2*B + bv*2*B + th*B + bh) * in_sram(c*s*B + th*s_over_2*B + d*B + bh) // 10 CU, 6 CU
+                            w_sram(v)(c, d, tv, bv, th, bh) * in_sram(c*s*B + d*2*B + th*B + bh) // 10 CU, 6 CU
                         }{_+_} // 14 CU
-                        tmp_out_sram(v)(c*s*B + tv*s_over_2*B + d*B + bv) = sum // 3 CU             
+                        tmp_out_sram(v)(c*s*B + d*2*B + tv*B + bv) = sum // 3 CU     
                     }                    
                 }
                 
@@ -168,13 +136,13 @@ class Pixelfly_1024_2_128 extends Pixelfly( // Pixelfly_N_B_batch
                     val s_over_2 = s_over_2_list(v)
                     val N_over_s = N_over_s_list(v)
                     
-                    Foreach(0 until N_over_s, 0 until 2, 0 until s_over_2, 0 until B par B, 0 until 2 par 2, 0 until B par B) { case Seq(c, tv, d, bv, th, bh) => // 21 CU
-                        val read_w = w_sram(v)(c*2*s*B*B + tv*s*B*B + d*B*2*B + bv*2*B + th*B + bh) // 10 CU
-                        val read_out = out_sram(c*s*B + tv*s_over_2*B + d*B + bv) // 5 CU
-                        val read_in = in_sram(c*s*B + th*s_over_2*B + d*B + bh) // 6 CU
+                    Foreach(0 until N_over_s, 0 until s_over_2, 0 until 2, 0 until B, 0 until 2 par 2, 0 until B par B) { case Seq(c, d, tv, bv, th, bh) => // 21 CU
+                        val read_w = w_sram(v)(c, d, tv, bv, th, bh) // 10 CU
+                        val read_in = in_sram(c*s*B + d*2*B + th*B + bh) // 6 CU
+                        val read_out = out_sram(c*s*B + d*2*B + tv*B + bv) // 5 CU
                         
-                        w_tmp_sram(v)(c*2*s*B*B + th*s*B*B + d*B*2*B + bh*2*B + tv*B + bv) = read_w * read_out // 10 CU     
-                        w_new_sram(v)(c*2*s*B*B + tv*s*B*B + d*B*2*B + bv*2*B + th*B + bh) = read_w - lr * read_in * read_out // 10 CU
+                        w_tmp_sram(v)(c, d, th, bh, tv, bv) = read_w * read_out // 10 CU     
+                        w_new_sram(v)(c, d, tv, bv, th, bh) = read_w - lr * read_in * read_out // 10 CU
                     }
                 }
                 
@@ -185,11 +153,11 @@ class Pixelfly_1024_2_128 extends Pixelfly( // Pixelfly_N_B_batch
                     val s_over_2 = s_over_2_list(v)
                     val N_over_s = N_over_s_list(v)
                     
-                    Foreach(0 until N_over_s, 0 until 2, 0 until s_over_2, 0 until B) { case Seq(c, th, d, bh) =>
+                    Foreach(0 until N_over_s, 0 until s_over_2, 0 until 2, 0 until B) { case Seq(c, d, th, bh) =>
                         val sum = Reduce(Reg[T])(0 until 2 par 2, 0 until B par B) { (tv, bv) =>
-                            w_tmp_sram(v)(c*2*s*B*B + th*s*B*B + d*B*2*B + bh*2*B + tv*B + bv) // 13 CU
+                            w_tmp_sram(v)(c, d, th, bh, tv, bv) // 13 CU
                         }{_+_} // 10 CU
-                        tmp_err_in_sram(v)(c*s*B + th*s_over_2*B + d*B + bh) = sum // 3 CU               
+                        tmp_err_in_sram(v)(c*s*B + d*2*B + th*B + bh) = sum // 3 CU               
                     }
                 }
                 
@@ -198,8 +166,12 @@ class Pixelfly_1024_2_128 extends Pixelfly( // Pixelfly_N_B_batch
                 
                 
                 for (v <- 0 to V-1) {
-                    Foreach(0 until N*B*2*B par ip) { i =>
-                        w_sram(v)(i) = w_new_sram(v)(i) // 5 CU
+                    val s = s_list(v)
+                    val s_over_2 = s_over_2_list(v)
+                    val N_over_s = N_over_s_list(v)
+                    
+                    Foreach(0 until N_over_s, 0 until s_over_2, 0 until 2, 0 until B, 0 until 2 par 2, 0 until B par B) { case Seq(c, d, tv, bv, th, bh) =>
+                        w_sram(v)(c, d, tv, bv, th, bh) = w_new_sram(v)(c, d, tv, bv, th, bh) // 5 CU
                     }
                 } 
                 
@@ -210,20 +182,15 @@ class Pixelfly_1024_2_128 extends Pixelfly( // Pixelfly_N_B_batch
                 
                 
                 
-                out(ba*N*B::(ba+1)*N*B par ip) store out_sram // 6 CU
-                err_in(ba*N*B::(ba+1)*N*B par ip) store err_in_sram // 6 CU
+                out(ba, 0::N*B par ip) store out_sram // 6 CU
+                err_in(ba, 0::N*B par ip) store err_in_sram // 6 CU
                     
-            }
-            
-            for (v <- 0 to V-1) {    
-                w_modified(v*N*B*2*B::(v+1)*N*B*2*B par ip) store w_sram(v) // 15 CU
             }
                  
         }
         
         writeCSV1D(getMem(out), data+"/out.csv", delim="\n")
         writeCSV1D(getMem(err_in), data+"/err_in.csv", delim="\n")
-        writeCSV1D(getMem(w_modified), data+"/w_modified.csv", delim="\n")
         
         assert(true)
     }
